3e2892fcf6555d930d1ccba94693c6133bcdebd0
Demidog03 <olzhasktl362@gmail.com>
2026-06-05 23:48:01 +0500

    products controller

--- the-scoville-inferno-api/start/routes.ts
+++ the-scoville-inferno-api/start/routes.ts
@@ -9,6 +9,10 @@
 
 import router from '@adonisjs/core/services/router'
 import { middleware } from './kernel.js'
+const ProductsController = () => import('#controllers/products_controller')
+const HeatLevelsController = () => import('#controllers/heat_levels_controller')
+const CategoriesController = () => import('#controllers/categories_controller')
+const BrandsController = () => import('#controllers/brands_controller')
 
 const AuthController = () => import('#controllers/auth_controller')
 
@@ -23,12 +27,32 @@ router
 // AUTH ROUTES
 router
   .group(() => {
-    router
+    ; ((((router
       .group(() => {
         router.post('/login', [AuthController, 'login'])
         router.post('/register', [AuthController, 'register'])
         router.get('/me', [AuthController, 'me']).use(middleware.auth({ guards: ['api'] }))
       })
-      .prefix('/auth')
+      .prefix('/auth'),
+      router
+        .group(() => {
+          router.get('/', [BrandsController, 'index'])
+        })
+        .prefix('/brands')),
+      router
+        .group(() => {
+          router.get('/', [CategoriesController, 'index'])
+        })
+        .prefix('/categories')),
+      router
+        .group(() => {
+          router.get('/', [HeatLevelsController, 'index'])
+        })
+        .prefix('/heat-levels')),
+      router
+        .group(() => {
+          router.get('/', [ProductsController, 'index'])
+        })
+        .prefix('/products'))
   })
   .prefix('/api/v1')
